9a0e7862e6bad3d0481977db4032de6043fac4c2
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-11-21 15:21:45 -0300

    model and Seed of Appointment created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# seed.rb
-# Event.destroy_all
-
+Appointment.destroy_all
 Event.destroy_all
 User.destroy_all
 
@@ -33,5 +22,15 @@ end
     event_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
     price: Faker::Number.within(range: 300..1000)
   )
-end
+
+  5.times do
+    Appointment.create!(
+      user_id: User.all.pluck(:id).sample,
+      event_id: Event.all.pluck(:id).sample,
+      timeslot: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    )
+  end
+puts "Created #{User.count} users"
 puts "Created #{Event.count} events"
+puts "Created #{Appointment.count} appointments"
+end
