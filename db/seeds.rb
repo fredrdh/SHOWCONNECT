3e3b8c210648fe92a0ab3c76eda53ffaa60266bb
Ismael-Soares bellsoares3@gmail.com <bellsoares3@gmail.com>
2023-11-21 14:33:08 -0300

    fiz setup inicial do projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# seed.rb
+# Event.destroy_all
+
+Event.destroy_all
+User.destroy_all
+
+5.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    cpf: Faker::Number.number(digits: 11),
+    password: "123123"
+  )
+end
+
+5.times do
+  Event.create!(
+    stage_name: Faker::Music.band,
+    category: ["concert", "convention", "music"].sample,
+    specialty: Faker::Music.genre,
+    user_id: User.all.pluck(:id).sample,
+    description: Faker::Lorem.paragraph,
+    local: Faker::Address.street_address,
+    event_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+    price: Faker::Number.within(range: 300..1000)
+  )
+end
+puts "Created #{Event.count} events"
