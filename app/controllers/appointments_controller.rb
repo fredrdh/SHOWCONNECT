fa9a9771166dc3175d4b488ac389523206b63b45
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-11-22 13:02:12 -0300

    controller appointments criado

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,43 @@
+class AppointmentsController < ApplicationController
+
+def index
+  @appointments = Appointment.all
+end
+
+def show
+  @appointment = Appointment.find(params[:id])
+end
+
+def new
+  @appointment = Appointment.new
+
+def create
+  @appointment = Appointment.new(appointment_params)
+  @appointment.user = current_user
+  if @appointment.save
+    redirect_to appointment_path(@appointment)
+  else
+    render :new
+  end
+
+def edit
+  @appointment = Appointment.find(params[:id])
+end
+
+def update
+  @appointment = Appointment.find(params[:id])
+  @appointment.update(appointment_params)
+  redirect_to appointment_path(@appointment)
+end
+
+def destroy
+  @appointment = Appointment.find(params[:id])
+  @appointment.destroy
+  redirect_to appointments_path
+end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:stage_name, :category, :specialty, :description, :local, :event_date, :price)
+  end
