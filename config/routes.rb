44cac617a638e87e414837355c0623c491f253af
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-11-22 11:03:34 -0300

    update event controller and login sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "posts#index"  # Defines the root path route ("/")
+  resources :events
 
-  resources :appointments, only: %i[create destroy]
-  resources :users, only: [:show]
-  resources :payments, only: %i[new create]
-
-  get "up" => "rails/health#show", as: :rails_health_check
-  get 'categorias', to: 'pages#categorias'
-  get 'pagamentos', to: 'pages#pagamentos'
+#   get "up" => "rails/health#show", as: :rails_health_check
+#   get 'categorias', to: 'pages#categorias'
+#   get 'pagamentos', to: 'pages#pagamentos'
 end
